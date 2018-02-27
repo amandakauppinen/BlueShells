d9894c67ea2a4809954392420ede3528357fec4e
Giovanni Legname <giovannl@metropolia.fi>
2018-02-27 15:45:46 +0200

    Created enemy and basic movement

---
+++ SeniorsGoneWild/Assets/Scripts/NurseController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NurseController : MonoBehaviour {
+
+	public float moveSpeed;
+
+	private Rigidbody2D myRigidBody;
+
+	private bool moving;
+
+	public float timeBetweenMove;
+	private float timeBewtweenMoveCounter;
+	public float timeToMove;
+	private float timeToMoveCounter;
+
+	private Vector3 moveDirection;
+
+
+	// Use this for initialization
+	void Start () {
+		myRigidBody = GetComponent<Rigidbody2D> ();
+
+		timeBewtweenMoveCounter = timeBetweenMove;
+		timeToMoveCounter = timeToMove;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (moving) {
+
+			timeToMoveCounter -= Time.deltaTime;
+			myRigidBody.velocity = moveDirection;
+
+			if (timeToMoveCounter < 0f) {
+				
+				moving = false;
+				timeBewtweenMoveCounter = timeBetweenMove;
+			}
+			
+		} else {
+			
+			timeBewtweenMoveCounter -= Time.deltaTime;
+			myRigidBody.velocity = Vector2.zero;
+
+			if (timeBewtweenMoveCounter < 0f) {
+
+				moving = true;
+				timeToMoveCounter = timeToMove;
+
+				moveDirection = new Vector3 (Random.Range (-1f, 1f) * moveSpeed, Random.Range(-1f, 1f)* moveSpeed, 0f) ;
+
+			}
+				
+		}
+
+	}
+
+}
