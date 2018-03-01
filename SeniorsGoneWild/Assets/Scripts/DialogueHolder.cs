6c11d61102f3b691d66320988c7c492b21492d52
Giovanni Legname <giovannl@metropolia.fi>
2018-03-01 10:22:16 +0200

    Added simple Dialogue scripts, gonna make them better

---
+++ SeniorsGoneWild/Assets/Scripts/DialogueHolder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueHolder : MonoBehaviour {
+
+
+	public string dialogue;
+	private DialogueManager dMan;
+	// Use this for initialization
+	void Start () {
+		dMan = FindObjectOfType<DialogueManager> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay2D(Collider2D other){
+		
+		if (other.gameObject.name == "Player")
+		{
+			
+			if (Input.GetKeyUp(KeyCode.Space))
+			{
+				dMan.ShowBox(dialogue);
+			}
+		}
+	}
+
+}
