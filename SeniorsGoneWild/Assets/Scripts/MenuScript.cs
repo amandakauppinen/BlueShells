f4d37d8cb6d0e716bfdf8fb6246bc39bc8a7bfca
Amanda Kauppinen <amandakauppinen3@gmail.com>
2018-03-02 12:22:23 +0200

    Added a basic title screen

---
+++ SeniorsGoneWild/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Title screen script
+/// </summary>
+public class MenuScript : MonoBehaviour
+{
+	public void StartGame()
+	{
+		Application.LoadLevel("Level 1");
+	}
+}
